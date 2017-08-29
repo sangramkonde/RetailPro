c1a76859dc43bc4447af7bdf4b461c233ac64c72
Sangram Konde <sangramkonde@gmail.com>
2017-08-29 06:55:48 +0530

    Correction in test cases for service

--- RetailManager/src/test/java/shop/retail/service/ShopLocatorTests.java
+++ RetailManager/src/test/java/shop/retail/service/ShopLocatorTests.java
@@ -93,7 +93,7 @@ public class ShopLocatorTests {
 	
 	@Test
 	public void testGetAllShop(){
-		List<Shop> toDoList = new ArrayList<Shop>();
+		List<Shop> shopList = new ArrayList<Shop>();
 		
 		Shop Mumbai = buildShopObject("Shop  at Mumbai", "Number 1", 400307,
 				73.9823, 18.5793);
@@ -108,13 +108,13 @@ public class ShopLocatorTests {
 		Shop Chennai = buildShopObject("Shop at Chennai", "Number 6", 7,
 				72.8777, 19.076);
 		
-		toDoList.add(Mumbai);
-		toDoList.add(Pune);
-		toDoList.add(Nashik);
-		toDoList.add(Delhi);
-		toDoList.add(Agra);
-		toDoList.add(Chennai);
-		when(retailShopDao.getAll()).thenReturn(toDoList);
+		shopList.add(Mumbai);
+		shopList.add(Pune);
+		shopList.add(Nashik);
+		shopList.add(Delhi);
+		shopList.add(Agra);
+		shopList.add(Chennai);
+		when(retailShopDao.getAll()).thenReturn(shopList);
 		
 		List<Shop> result = shopLocator.getAll();
 		assertEquals(6, result.size());
@@ -133,7 +133,7 @@ public class ShopLocatorTests {
 	
 	@Test
 	public void testNearest() {
-		List<Shop> toDoList = new ArrayList<Shop>();
+		List<Shop> shopList = new ArrayList<Shop>();
 		Shop Mumbai = buildShopObject("Shop  at Mumbai", "Number 1", 400307,
 				73.9823, 18.5793);
 		Shop Pune = buildShopObject("Shop at Pune", "Number 2", 900, 73.8796,
@@ -147,14 +147,14 @@ public class ShopLocatorTests {
 		Shop Chennai = buildShopObject("Shop at Chennai", "Number 6", 7,
 				72.8777, 19.076);
 		
-		toDoList.add(Mumbai);
-		toDoList.add(Pune);
-		toDoList.add(Nashik);
-		toDoList.add(Delhi);
-		toDoList.add(Agra);
-		toDoList.add(Chennai);
+		shopList.add(Mumbai);
+		shopList.add(Pune);
+		shopList.add(Nashik);
+		shopList.add(Delhi);
+		shopList.add(Agra);
+		shopList.add(Chennai);
 		
-		when(shopLocator.getAll()).thenReturn(toDoList);
+		when(shopLocator.getAll()).thenReturn(shopList);
 		Shop shop =shopLocator.findNearest("22.5726", "88.3639");
 		assertEquals(shop.getShopName(),Delhi.getShopName());
 		
@@ -206,133 +206,4 @@ public class ShopLocatorTests {
 			return false;
 		}
 	}
-	
-/*	@Autowired
-	private RetailShopDao retailShopDao;
-	LatLng locationToTest = null;
-
-
-
-	@Before
-	public void setUp() {
-		// retailShopDao = Mockito.mock(RetailShopDao.class);
-		// shopLocator = new ShopLocator();
-	}
-
-	
-	 * private Shop buildShopObject(String name, String number, int post, Double
-	 * latitude, Double longitude) { Shop shop = new Shop(); ShopAddress
-	 * shopAddress = new ShopAddress(); shop.setShopName(name);
-	 * shopAddress.setShopLatitude(latitude);
-	 * shopAddress.setShopLongitude(longitude); shopAddress.setPostCode(post);
-	 * shopAddress.setNumber(number); shop.setShopAddress(shopAddress); return
-	 * shop; }
-	 * 
-	 * @Test public void testAddShop() { Shop shop = buildShopObject(
-	 * "Masemari",
-	 * "326/2, Bawdhan (Budruk), Mumbai-Bangalore Highway, Bhunde Vasti, Bavdhan, Pune, Maharashtra"
-	 * , 411021, 18.5185887, 73.7659984);
-	 * Mockito.when(retailShopDao.addShop(Mockito
-	 * .any(Shop.class))).thenReturn(shop); Shop newShop =
-	 * shopLocator.addShop(shop); assertEquals(shop.getShopName(),
-	 * newShop.getShopName()); }
-	 
-
-//	LatLng locationToTest = null;
-
-	@Before
-	public void setData() throws Exception {
-		locationToTest = setLocationFromGoogleApi("Pune - Bengaluru Hwy, Pune"
-				.concat(",").concat(411014 + ""));
-	}
-
-	@Test
-	public void testGeoApiResolver() {
-		String address = "Pune - Bengaluru Hwy, Pune".concat(",").concat(
-				411014 + "");
-		LatLng location = ShopLocatorImpl.geoApiResolver(address);
-		assert (isSameLocation(location, locationToTest));
-	}
-
-	@Test
-	public void testSave() {
-		Shop shop = buildShopObject("DMART Shop", "Pune - Bengaluru Hwy, Pune",
-				411009, locationToTest.lat, locationToTest.lng);
-
-		shopLocator.addShop(shop);
-		assert (shopLocator.getAll().get(0).equals(shop));
-	}
-
-	@Test
-	public void testNearest() {
-		Shop Mumbai = buildShopObject("Shop  at Mumbai", "Number 1", 400307,
-				73.9823, 18.5793);
-		Shop Pune = buildShopObject("Shop at Pune", "Number 2", 900, 73.8796,
-				18.5529);
-		Shop Nashik = buildShopObject("Shop at Nashik", "Number 3", 83,
-				73.7997, 18.6298);
-		Shop Delhi = buildShopObject("Shop at Delhi", "Number 4", 545678,
-				88.3639, 22.5726);
-		Shop Agra = buildShopObject("Shop at Agra", "Number 5", 1, 77.4126,
-				23.2599);
-		Shop Chennai = buildShopObject("Shop at Chennai", "Number 6", 7,
-				72.8777, 19.076);
-		
-		shopLocator.addShop(Mumbai);
-		shopLocator.addShop(Pune);
-		shopLocator.addShop(Nashik);
-		shopLocator.addShop(Delhi);
-		shopLocator.addShop(Agra);
-		shopLocator.addShop(Chennai);
-		
-//		shopInMemoryArray.add(Mumbai);
-//		shopInMemoryArray.add(Pune);
-//		shopInMemoryArray.add(Nashik);
-//		shopInMemoryArray.add(Delhi);
-//		shopInMemoryArray.add(Agra);
-//		shopInMemoryArray.add(Chennai);
-
-		// Latitude and Longitude of m Pimpari is 73.9143 18.5679
-		// Nearest shop from Pimpari should be at Pune 
-		assert shopLocator.findNearest(new LatLng(73.9143,18.5679)).equals(Pune);
-		// Latitude and Longitude of Thane is 75.3433 19.8762
-		// Nearest shop from Thane should be at Mumbai
-		assert shopLocator.findNearest(new LatLng(75.3433,19.8762)).equals(Mumbai);
-		// Latitude and Longitude of Ramapuram is 72.8777 19.076
-		// Nearest shop from Ramapuram should be at Chennai itself 
-		assert shopLocator.findNearest(new LatLng(72.8777, 19.076)).equals(Chennai);
-	}
-
-	private Shop buildShopObject(String name, String number, int post,
-			Double latitude, Double longitude) {
-		Shop shop = new Shop();
-		ShopAddress shopAddress = new ShopAddress();
-		shop.setShopName(name);
-		shopAddress.setNumber(number);
-		shopAddress.setPostCode(post);
-		shopAddress.setShopLatitude(latitude);
-		shopAddress.setShopLongitude(longitude);
-		shop.setShopAddress(shopAddress);
-		return shop;
-	}
-
-	private LatLng setLocationFromGoogleApi(String address) throws Exception {
-		GeoApiContext context = new GeoApiContext()
-				.setApiKey(Config.GEO_API_KEY);
-		GeocodingResult result = GeocodingApi.geocode(context, address).await()[0];
-		LatLng location = result.geometry.location;
-		return location;
-	}
-
-	private boolean isSameLocation(LatLng l1, LatLng l2) {
-		if (l1 == null || l2 == null) {
-			return false;
-		}
-		if (l1.lat == l2.lat && l1.lng == l2.lng) {
-			return true;
-		} else {
-			return false;
-		}
-	}*/
-
 }
